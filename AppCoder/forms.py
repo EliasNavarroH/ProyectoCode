4ebcfaa21ca3da1aa5ed6a2a3207623d562e5dee
Huelmig <eliasnvr@icloud.com>
2023-01-30 12:27:29 -0300

    Se añadio la busqueda de cursos con un nuevo view, un html y url

--- AppCoder/forms.py
+++ AppCoder/forms.py
@@ -3,4 +3,4 @@ from django import forms
 
 class CursoFormulario(forms.Form):
      nombre = forms.CharField(max_length=64)
-     camada = forms.IntegerField(required=True) #si no se envia la camada, el formulario envia error
+     camada = forms.IntegerField(required=True, max_value=2000) #si no se envia la camada, el formulario envia error
